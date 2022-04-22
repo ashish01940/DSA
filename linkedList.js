95fe3a67d038bc940be5a952309641dc1626a9fb
ashishsaini <ashish.saini@cloudesign.com>
2022-04-23 00:38:39 +0530

    just started

---
+++ linkedList.js
@@ -0,0 +1,15 @@
+class node{
+    constructor(value){
+        this.element = value;
+        this.next = null;
+    }
+}
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.length = 0;
+    }
+    addItem(value){
+        
+    }
+}
